a3e32438e78f678bfc120bd64d4c39dea47b6891
Nicholas Hart <nic@hdb3.net>
2018-12-19 17:08:16 +0000

    preserving MRTquest.v1.hs incase the enum route is sticky...

--- MRTquest.v1.hs
+++ MRTquest.v1.hs
@@ -20,21 +20,28 @@ main = do
 
 collect = foldl f Map.empty 
     where
-    f m v = Map.insertWith f' (identify' v) [v] m
+    f m v = Map.insertWith f' (identify v) [v] m
     f' [u] ux = u:ux 
 
-identify MRTlib.MRTPeerIndexTable{..} = MRTPeerIndexTable
-identify MRTlib.RIBIPV4Unicast{..} = RIBIPV4Unicast
-identify MRTlib.RIBIPV6Unicast{..} = RIBIPV6Unicast
-identify MRTlib.MRTUnimplemented{..} = MRTUnimplemented
-identify MRTlib.BGP4MPMessageAS4{..} = BGP4MPMessageAS4
-identify MRTlib.BGP4MPStateChangeAS4{..} = BGP4MPStateChangeAS4
-identify MRTlib.RIBv1IPv4{..} = RIBv1IPv4
-identify MRTlib.RIBv1IPv6{..} = RIBv1IPv6
-identify' = fromEnum . identify
+identify MRTlib.MRTPeerIndexTable{..} = 0
+identify MRTlib.RIBIPV4Unicast{..} = 1
+identify MRTlib.RIBIPV6Unicast{..} = 2
+identify MRTlib.MRTUnimplemented{..} = 3
+identify MRTlib.BGP4MPMessageAS4{..} = 4
+identify MRTlib.BGP4MPStateChangeAS4{..} = 5
+identify MRTlib.RIBv1IPv4{..} = 6
+identify MRTlib.RIBv1IPv6{..} = 7
+unidentify n = mrtNames !! n
 
-unidentify :: Int -> MRTTypes
-unidentify = toEnum
+mrtNames = [ "MRTPeerIndexTable"
+           ,"RIBIPV4Unicast"
+           ,"RIBIPV6Unicast"
+           ,"MRTUnimplemented"
+           ,"BGP4MPMessageAS4"
+           ,"BGP4MPStateChangeAS4"
+           ,"RIBv1IPv4"
+           ,"RIBv1IPv6"
+           ]
 
 data MRTTypes = MRTPeerIndexTable|RIBIPV4Unicast|RIBIPV6Unicast|MRTUnimplemented|BGP4MPMessageAS4|BGP4MPStateChangeAS4|RIBv1IPv4|RIBv1IPv6 deriving (Show,Enum)
 -- mrtFilter types = 
