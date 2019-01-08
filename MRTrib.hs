5a114b94748017edd7e334c8e49c9d469fc1fd04
Nicholas Hart <nic@hdb3.net>
2019-01-08 15:31:03 +0000

    staring PrefixGroupMetrics.hs

--- MRTrib.hs
+++ MRTrib.hs
@@ -8,7 +8,7 @@ module MRTrib ( IP4Prefix,IP6Prefix
               , RouteMapv4,RouteMapv6
               , MRTRibV4,MRTRibV6
               , getMRTTableDumpV2
-              , v4hash, v4unhash, v6hash, v6unhash) where
+              , v4hash, v4unhash, v6hash, v6unhash, IP4PrefixHash, IP6PrefixHash ) where
 
 import Data.Bits
 import Data.IP
@@ -27,14 +27,16 @@ instance Data.Hashable.Hashable IPv4
 instance Data.Hashable.Hashable IPv6
 instance Data.Hashable.Hashable IPPrefix
 
+type IP4PrefixHash = Int
+type IP6PrefixHash = Int
 
-v4hash :: IP4Prefix -> Int
+v4hash :: IP4Prefix -> IP4PrefixHash
 v4hash (ip,l) = let w64 x = fromIntegral x :: Word64 in fromIntegral $ unsafeShiftL (w64 l) 32 .|. w64 ( byteSwap32 $ toHostAddress ip)
 
-v4unhash :: Int -> IP4Prefix
+v4unhash :: IP4PrefixHash -> IP4Prefix
 v4unhash h = ( fromHostAddress $ byteSwap32 $ fromIntegral $ 0xffffffff .&. h' , fromIntegral $ unsafeShiftR h' 32 ) where h' = fromIntegral h :: Word64
 
-v6unhash :: Int -> IP6Prefix
+v6unhash :: IP6PrefixHash -> IP6Prefix
 v6unhash h = ( fromHostAddress64 $ 0xffffffffffffff .&. h' , fromIntegral $ unsafeShiftR h' 56)
     where
     h' = fromIntegral h :: Word64
@@ -42,7 +44,7 @@ v6unhash h = ( fromHostAddress64 $ 0xffffffffffffff .&. h' , fromIntegral $ unsa
         w0 = fromIntegral $ unsafeShiftR x 32
         w1 = fromIntegral $ x .&. 0xffffffff
 
-v6hash :: IP6Prefix -> Int
+v6hash :: IP6Prefix -> IP6PrefixHash
 v6hash (ip,l) | l < 57 = fromIntegral $ unsafeShiftL (w64 l) 56 .|. toHostAddress64 ip
             -- | otherwise = fromIntegral $ asWord64 $ hash64WithSeed (w64 l) (toHostAddress64 ip) 
               | otherwise = error $ "cant' handle IPv6 > /56: " ++ show l ++ "/" ++ show ip
