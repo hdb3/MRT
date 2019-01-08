00e9eb0a570784f52913117815d87c1f44df06fe
Nicholas Hart <nic@hdb3.net>
2019-01-08 16:14:45 +0000

    PrefixGroupMetrics.hs - lookup compiles

--- PrefixGroupMetrics.hs
+++ PrefixGroupMetrics.hs
@@ -1,7 +1,8 @@
 module PrefixGroupMetrics where
 
 import qualified Data.IntMap.Strict as Map
-import Data.Maybe(isJust)
+import Data.Maybe(isJust,catMaybes)
+import Data.List(sort,group)
 import MRTrib
 import MRTrib
 
@@ -9,25 +10,23 @@ newtype PrefixRib = PR (Map.IntMap PrefixListHash)
 newtype PrefixCache = PC (Map.IntMap IP4PrefixList)
 
 lookup :: (PrefixRib, PrefixCache) -> (IP4PrefixList,PrefixListHash) -> [Int]
-lookup (PR pr, PC pc) (pl,plh) = if isJust $ Map.lookup plh pc then [length pl] else []
+lookup (PR pr, PC pc) (pl,plh) = if isJust $ Map.lookup plh pc then [length pl] else reduce $ map ((flip Map.lookup) pr . v4hash) pl where
+    reduce :: [Maybe Int] -> [Int]
+    reduce = sort . map length . group . sort . catMaybes
 
 buildRIB :: Map.IntMap (a, IP4PrefixList) -> (PrefixRib, PrefixCache)
 buildRIB m = (buildPrefixRib prefixRibItems, buildPrefixCache prefixCacheItems)
     where
     prefixLists = map snd $ Map.elems m
     prefixRibItems :: [(IP4PrefixHash,PrefixListHash)]
-    prefixRibItems = concatMap (\prefixList -> map (\prefix -> (hashP prefix, hashL prefixList)) prefixList) prefixLists
+    prefixRibItems = concatMap (\prefixList -> map (\prefix -> (v4hash prefix, prefixHash prefixList)) prefixList) prefixLists
     ---prefixRibItems = concatMap f prefixLists
     ---f :: IP4PrefixList -> [(IP4PrefixHash,PrefixListHash)]
-    ---f pl = map (\prefix -> (hashP prefix, hashL pl)) pl
+    ---f pl = map (\prefix -> (v4hash prefix, prefixHash pl)) pl
     buildPrefixRib :: [(IP4PrefixHash,PrefixListHash)] -> PrefixRib
     buildPrefixRib = PR . Map.fromList
 
     prefixCacheItems :: [(PrefixListHash,IP4PrefixList)]
-    prefixCacheItems = map (\prefixList -> (hashL prefixList, prefixList)) prefixLists
+    prefixCacheItems = map (\prefixList -> (prefixHash prefixList, prefixList)) prefixLists
     buildPrefixCache :: [(PrefixListHash,IP4PrefixList)] -> PrefixCache
     buildPrefixCache = PC . Map.fromList
-
-    hashP = v4hash
-    hashL :: IP4PrefixList -> PrefixListHash
-    hashL = prefixHash
